e848706d4122568bb58b8d1a0446613f020997a5
DESKTOP-NDQK5IR\Bruker1 <nilofarrazi@hotmail.com>
2021-01-28 08:39:15 +0100

    test 3

---
+++ src/AdditionArray.java
@@ -0,0 +1,8 @@
+public class App {
+    public static int add(int[] numbers){
+        int s = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            s += numbers[i];
+        }
+    }
+}
